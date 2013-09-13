1ea96095098b6980ef0833db51ea799d8d891175
The Greatest <shaunraj@gmail.com>
2013-09-13 09:44:38 -0700

    Revert "Revert "removed file""
    This reverts commit 334dd4e1f198eccfc10cdac1af3ccff30c5676c0.

--- shell_scripts/config_xbee.sh
+++ shell_scripts/config_xbee.sh
@@ -3,7 +3,8 @@ sudo stty 9600 cs8 </dev/ttyUSB0
 echo "Finished Here"
 echo -en "+++" > /dev/ttyUSB0 
 echo "finished sending command"
-while read -r line < /dev/ttyUSB0; do
-	echo $result > /dev/ttyUSB0
-done
+#while read -r line < /dev/ttyUSB0; do
+#	echo $result > /dev/ttyUSB0
+#done
+cat -v /dev/ttyUSB0 > output
 echo "done"
