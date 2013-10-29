305fb167acd8624804a64e27bdf2ec9b02e2c2b4
The Greatest <shaunraj@gmail.com>
2013-10-28 19:17:19 -0700

    HUUGE Commit inbound

--- BBB/wireless_connect.js
+++ BBB/wireless_connect.js
@@ -3,7 +3,6 @@ var fs = require('fs')
 var exec = require('child_process').exec
 
 var connJSON
-var connType;
 
 function terminal_output(command, callback){
 	exec(command, function(error, stdout, stderr){
@@ -11,12 +10,61 @@ function terminal_output(command, callback){
 	});
 }
 
-function wireless_connect(jsonIn, type){
-	connJSON = JSON.parse(jsonIn);
-	connType = type;
+function wireless_connect(jsonIn){
+	connJSON = jsonIn
 } 
 
 wireless_connect.prototype.connect = function(callback){
-	if (connType == 'DHCP'){
-		terminal_output('wpa_cli add_network		
-		
+	terminal_output('touch wpa_supplicant.conf', function(stdout){
+		console.log('wpa_supplicant.conf created');
+	});
+	
+
+	if (connJSON.security.match(/WEP/)){
+		var stream = fs.createWriteStream("/etc/wpa_supplicant.conf");
+		stream.once('open', function(close){
+			stream.write('ctrl_interface=/var/run/wpa_supplicant\n');
+			stream.write('ctrl_interface_group=0\n');
+			stream.write('update_config=1\n');
+			stream.write('network={\n');
+			stream.write('\tssid="'+connJSON.ssid+'"\n');
+			stream.write('\tkey_mgmt=NONE\n');
+			stream.write('\twep_key0='+connJSON.password+'\n');
+			stream.write('}')
+			stream.end();
+		});
+	}
+
+	if (connJSON.security.match(/WPA/)){
+		var stream = fs.createWriteStream('/etc/wpa_supplicant.conf');
+		stream.once('open', function(close){
+			stream.write('ctrl_interface=/var/run/wpa_supplicant\n');
+			stream.write('ctrl_interface_group=0\n');
+			stream.write('update_config=1\n');
+			stream.write('network={\n');
+			stream.write('\tssid="'+connJSON.ssid+'"\n');
+			stream.write('\tkey_mgmt='+connJSON.security+'\n');
+			console.log('HERE');
+			if(connJSON.security.match(/PSK/)){
+				stream.write('\tpsk="'+connJSON.password+'"\n');
+			}
+			if(connJSON.group){
+				stream.write('\tgroup='+connJSON.group+'\n');
+			}
+			stream.write('}');
+			terminal_output('wpa_supplicant -Dwext -iwlan0 -c /etc/wpa_supplicant.conf -B', function(stdout){
+				console.log('Is it hanging...?');
+				terminal_output('udhcpc -i wlan0', function(stdout){
+					console.log('Please do not hang :3');
+				});
+			});
+		});
+	}
+}
+module.exports = wireless_connect
+//Test the module
+//var JSONin = {"ssid":"RajNetwork","password":"e65d7a1414e6e34bc874ebdb69", "security":"WEP"}
+var JSONin = {"ssid":"traegalia","password":"ADAB1C21BD82347205BB3B0156","security":"WPA-PSK"};
+var Connect = new wireless_connect(JSONin)
+Connect.connect();
+			
