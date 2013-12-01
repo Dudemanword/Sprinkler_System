f574a4c9f93cd8ef1a35ae7ff8e11be731b25c6d
The Greatest <shaunraj@gmail.com>
2013-12-01 16:56:09 -0700

    FINALLY added a timeout for connections

--- BBB/on_bone/wireless_connect.js
+++ BBB/on_bone/wireless_connect.js
@@ -1,12 +1,33 @@
 var sys = require('sys')
 var fs = require('fs')
 var exec = require('child_process').exec
+var spawn = require('child_process').spawn
+
 
 var connJSON
 
-function terminal_output(command, callback){
-	exec(command, function(error, stdout, stderr){
-		console.log(error,stdout,stderr);
+function terminal_output(/*command,options,callback*/){
+
+	_default = { encoding: 'utf8', timeout: 0,maxBuffer: 200*1024,killSignal: 'SIGTERM',cwd: null,env: null }
+
+	command = arguments[0];
+
+	console.log('type of argument is ' + typeof arguments[2])
+
+	if((typeof arguments[1]) == 'object'){
+		options = arguments[1];
+		callback = arguments[2];
+	}
+
+	else if ((typeof arguments[1]) == 'function'){
+		options = _default;
+		callback = arguments[1];
+	}
+	
+	else
+		callback(new Error ('Give me some legit parameters...'));
+
+	exec(command,options, function(error, stdout, stderr){
 		callback(error, stdout, stderr);
 	});
 }
@@ -39,7 +60,7 @@ stream.write('ctrl_interface_group=0\n');
 				console.log('Is it hanging...?');
 				terminal_output('sudo /sbin/udhcpc -i wlan0', function(error, stdout, stderr){
 					console.log('Please do not hang :3')
-n				});
+				});
 			});
 		});
 	}
@@ -49,7 +70,6 @@ n				});
 		stream.once('open', function(close){
 			stream.write('ctrl_interface=/var/run/wpa_supplicant\n');
 			stream.write('ctrl_interface_group=0\n');
-//			stream.write('ctrl_interface_group=nodectrl\n');
 			stream.write('update_config=1\n');
 			stream.write('network={\n');
 			stream.write('\tssid="'+connJSON.ssid+'"\n');
@@ -62,7 +82,6 @@ n				});
 			}
 			if (connJSON.bssid)
 				stream.write('\tbssid='+connJSON.bssid+'\n');
-			console.log('HERE');
 			if(connJSON.security.match(/PSK/)){
 				stream.write('\tpsk="'+connJSON.password+'"\n');
 			}
@@ -71,12 +90,10 @@ n				});
 			}
 			stream.write('}');
 			stream.end();
-			console.log('Password?\n');
-			terminal_output('sudo /usr/sbin/wpa_supplicant -Dwext -iwlan0 -c ./wpa_supplicant.conf -B', function(error, stdout, stderr){
+			terminal_output('sudo /usr/sbin/wpa_supplicant -Dwext -iwlan0 -c ./wpa_supplicant.conf -B',{ encoding: 'utf8',timeout: 40000,maxBuffer: 200*1024,killSignal: 'SIGTERM',cwd: null,env: null }, function(error, stdout, stderr){
 				console.log(error, stdout, stderr);
 				if(error | stderr) throw error;
-				console.log('Pass Here?');
-				terminal_output('sudo /sbin/udhcpc -i wlan0', function(error, stdout, stderr){
+				terminal_output('sudo /sbin/udhcpc -t 3 -i wlan0' ,{ encoding: 'utf8',timeout: 30000,maxBuffer: 200*1024,killSignal: 'SIGTERM',cwd: null,env: null }, function(error, stdout, stderr){
 					console.log(error, stdout, stderr);
 					console.log('Please do not hang :3');
 				});
@@ -87,8 +104,8 @@ n				});
 module.exports = wireless_connect
 //Test the module
 //var JSONin = {"ssid":"RajNetwork","password":"e65d7a1414e6e34bc874ebdb69", "security":"WEP"}
-//var JSONin = {"ssid":"traegalia","password":"ADAB1C21BD82347205BB3B0156","security":"WPA-PSK"};
-var JSONin = {"ssid":"UCCS-Wireless","username":"sraj2", "password":"Iwashere1234", "special":"PEAP", "eaptype":"GTC", "security":"WPA-EAP"};
+var JSONin = {"ssid":"traegalia","password":"ADAB1C21BD82347205BB3B0156","security":"WPA-PSK"};
+//var JSONin = {"ssid":"UCCS-Wireless","username":"sraj2", "password":"Iwashere1234", "special":"PEAP", "eaptype":"GTC", "security":"WPA-EAP"};
 var Connect = new wireless_connect(JSONin)
 Connect.connect();
 /*setInterval(function(){
