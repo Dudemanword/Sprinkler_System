2d2d4608feff6c6972a42a7a7d835cce12efb0dd
The Greatest <shaunraj@gmail.com>
2013-12-01 23:44:15 -0700

    Fixed bug where wpa connection was improperly displayed

--- BBB/on_bone/wireless_json.js
+++ BBB/on_bone/wireless_json.js
@@ -10,7 +10,7 @@ function wireless_scan(_interface){
 	}
 
 wireless_scan.prototype.getData = function(callback){
-	exec("iwlist wlan0 scanning | egrep 'Cell |Encryption|Quality|Last beacon|ESSID|(IE: WPA|WEP)|(Signal level)'", function(error, stdout, stderr){
+	exec("iwlist wlan0 scanning | egrep 'Cell |Encryption|Quality|Last beacon|ESSID|((IE: WPA(2)?)|WPA(2)?|WEP)|(Signal level)'", function(error, stdout, stderr){
 		callback(stdout);
 	});
 }
@@ -29,16 +29,18 @@ wireless_scan.prototype.parseData = function(data, callback){
 	data = data.split(/Cell /g)
 	obj_arr = []
 	data.shift()
-
-	var encryption = /(WEP|WPA)/g
+	console.log(data);
+	var encryption = /(WEP|(WPA2?))/g
 	var bssid = /[0-9a-zA-z]{1,2}:[0-9a-zA-z]{1,2}:[0-9a-zA-z]{1,2}:[0-9a-zA-z]{1,2}:[0-9a-zA-z]{1,2}:[0-9a-zA-z]{1,2}/g
 	var essid = /ESSID:"[<a-zA-Z0-9_\->\.]+"/g
 	var sig_level = /Signal level=[0-9]+/g
 	for (info in data){
 		data[info] = data[info].replace(/\n/g, '');
-		data_enc = data[info].match(encryption);
+		data_enc = (data[info].match(encryption))
 		if(data_enc == null)
 			data_enc = 'N/A';
+		if(data_enc.toString().match(/WPA/g))
+			data_enc = data_enc.toString().replace(/WPA(2)?/,'WPA').replace(/\,WPA2/,'');
 		data_enc = data_enc.toString()
 		data_bssid = data[info].match(bssid).toString();
 		data_ssid = (data[info].match(essid)).toString().replace(/ESSID:/g,'').replace(/"/g,'');
